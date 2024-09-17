06708a878a7db840b05e3961b103bd5dc8dd5cf2
KrisHHFI <kristopherpepperuk@gmail.com>
2024-09-17 16:41:07 +0300

    Company page content 2

--- lib/widgets/company_page_content.dart
+++ lib/widgets/company_page_content.dart
@@ -9,51 +9,53 @@ class CompanyPageContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        // Determine a scalable font size with min and max limits
-        double fontSize = (constraints.maxWidth * 0.08).clamp(50.0, 70.0);
+        double mainTextfontSize =
+            (constraints.maxWidth * 0.02).clamp(50.0, 70.0);
+
+        double subTextfontSize =
+            (constraints.maxWidth * 0.02).clamp(15.0, 30.0);
 
         return Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Wrap the Row in a SingleChildScrollView for horizontal scrolling if needed
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // Map through the textItems list to generate the text widgets
-                    ...textItems.map((item) => Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 10.0),
-                          width: constraints.maxWidth *
-                              0.25, // Adjust width as needed
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                item['mainText']!,
-                                style: TextStyle(
-                                  fontSize: fontSize,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                                textAlign: TextAlign.center,
+              // Use Wrap instead of Row to handle overflow
+              Wrap(
+                alignment: WrapAlignment.center,
+                spacing: 20.0, // Horizontal space between items
+                runSpacing: 20.0, // Vertical space between lines
+                children: [
+                  // Map through the textItems list to generate the text widgets
+                  ...textItems.map((item) => Container(
+                        width: constraints.maxWidth *
+                            0.25, // Adjust width as needed
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              item['mainText']!,
+                              style: TextStyle(
+                                fontSize: mainTextfontSize,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
                               ),
-                              const SizedBox(height: 10),
-                              Text(
-                                item['subText']!,
-                                style: const TextStyle(
-                                  fontSize: 24,
-                                  color: Colors.white,
-                                ),
-                                textAlign: TextAlign.center,
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              item['subText']!,
+                              style: TextStyle(
+                                fontSize: subTextfontSize,
+                                color: Colors.white,
                               ),
-                            ],
-                          ),
-                        )),
-                  ],
-                ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      )),
+                ],
               ),
+              const SizedBox(height: 20), // Space between text and button
               const ChangePageButton(
                 buttonText: 'Interested?',
                 pageToChangeTo: 'Services',
