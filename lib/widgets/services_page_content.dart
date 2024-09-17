e7824e6b81f6dfaf3346b87a79c25056ad7542e0
KrisHHFI <kristopherpepperuk@gmail.com>
2024-09-17 22:28:30 +0300

    Services page progress 2

--- lib/widgets/services_page_content.dart
+++ lib/widgets/services_page_content.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../constants.dart';
 import './change_page_button.dart';
+import './custom_card.dart';
 
 class ServicesPageContent extends StatelessWidget {
   const ServicesPageContent({super.key});
@@ -26,32 +27,26 @@ class ServicesPageContent extends StatelessWidget {
                 alignment: WrapAlignment.center,
                 spacing: horizontalSpacing,
                 children: [
-                  // Map through the textItems list to generate the text widgets
-                  ...textItems.map((item) => Container(
-                        width: 160, // Fixed width for each text box
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              item['mainText']!,
-                              style: TextStyle(
-                                fontSize: mainTextfontSize,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                            Text(
-                              item['subText']!,
-                              style: TextStyle(
-                                fontSize: subTextfontSize,
-                                color: Colors.white,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        ),
-                      )),
+                  CustomCard(
+                    imageUrl:
+                        'images/CargoShip.jpg', // Credit: Pexels, Martin Damboldt, Photography of Ship
+                    text: 'Lorem ipsum dolor sit amet, consectetur.',
+                  ),
+                  CustomCard(
+                    imageUrl:
+                        'images/Engineers.jpg', // Credit: Pexels, Anamul Rezwan, Two Man Holding White Paper
+                    text: 'Duis aute irure dolor in reprehenderit in.',
+                  ),
+                  CustomCard(
+                    imageUrl:
+                        'images/Consultants.jpg', // Credit: Pexels, Sora Shimazaki, Ethnic businessman shaking hand of applicant in office
+                    text: 'Lorem ipsum dolor sit amet, consectetur.',
+                  ),
+                  CustomCard(
+                    imageUrl:
+                        'images/OfficePeople.jpg', // Credit: Pexels, LinkedIn Sales Navigator, Man Pointing Laptop Computer
+                    text: 'Duis aute irure dolor in reprehenderit in.',
+                  ),
                 ],
               ),
               const ChangePageButton(
