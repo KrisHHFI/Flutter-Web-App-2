8be07d28f687c2acb47c09bf5b9de279deff87d9
KrisHHFI <kristopherpepperuk@gmail.com>
2024-09-18 22:28:56 +0300

    Contact page progress 2

--- lib/widgets/contact_page_content.dart
+++ lib/widgets/contact_page_content.dart
@@ -1,3 +1,4 @@
+import 'dart:math' as math; // Import the math library for max function
 import 'package:flutter/material.dart';
 import './change_page_button.dart';
 
@@ -11,9 +12,15 @@ class ContactPageContent extends StatelessWidget {
         // Determine a scalable font size with min and max limits for the form title
         double fontSize = (constraints.maxWidth * 0.08).clamp(30.0, 50.0);
 
+        // Set the container width to 30vw or a minimum of 350 pixels
+        double containerWidth = math.max(constraints.maxWidth * 0.3, 350.0);
+
         return Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
+          child: Container(
+            width:
+                containerWidth, // Set the width to 30vw or 350px, whichever is larger
+            padding:
+                const EdgeInsets.all(16.0), // Add padding for internal spacing
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -93,7 +100,7 @@ class ContactPageContent extends StatelessWidget {
                           fillColor: Colors.black.withOpacity(0.5),
                         ),
                         style: const TextStyle(color: Colors.white),
-                        maxLines: 4, // Multi-line text field for the message
+                        maxLines: 2, // Multi-line text field for the message
                       ),
                       const SizedBox(height: 20),
 
